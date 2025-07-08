c7b1502596a62802b75f65dbe2023165ccba3838
Josue Rodriguez <jrodr1309@fiu.edu>
2025-07-08 11:37:29 -0400

    Update default scalings for plotting functions

--- src/plotting.py
+++ src/plotting.py
@@ -1,11 +1,11 @@
 
 
-def plot_eegdata(raw, duration=15, scalings=0.05, time_format="clock"):
+def plot_eegdata(raw, duration=15, scalings=10e-5, time_format="clock"):
     """Plot continous eegdata recording"""
     raw.plot(duration=duration, scalings=scalings, show_scrollbars=False, time_format=time_format)
 
 
-def plot_epochs(epochs, n_epochs, scalings=0.05):
+def plot_epochs(epochs, n_epochs, scalings=10e-5):
     """Plot epochs eegdata recording"""
     epochs.plot(n_epochs=n_epochs, scalings=scalings, show_scrollbars=False)
     
